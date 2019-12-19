1e18ec5deea0552b71a928f04969b2b6a57b0949
Jordi Prats <jordi.prats@gmail.com>
2019-12-19 14:50:44 +0100

    demo pre post scripts

---
+++ examples/demo_pre_post.pp
@@ -0,0 +1,10 @@
+class { 'tarbackup': }
+
+tarbackup::instance { 'backup':
+  includedir  => [ '/etc/logstash', '/etc/elasticsearch', '/etc/kibana' ],
+  xdev        => true,
+  destination => '/var/backup/tarbackup',
+  mailto      => 'demo@demo.com'
+  prescript   => '/bin/true',
+  postscript  => '/bin/true',
+}
